d7a717b3e79187d95e33da56a7c40fd4eabf5874
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-08-14 11:59:27 -0300

    modificada apresentação das strings

--- desafio.py
+++ desafio.py
@@ -18,39 +18,57 @@ while True:
     if opcao == "1":
         deposito = float(input(f"Informe o valor do depósito: \n"))
         saldo += deposito
-        extrato += f"\nDepósito: R${deposito:.2f}"
+        extrato += f"\n- Depósito: R${deposito:.2f}"
+        print("\nDepósito realizado com sucesso!\n")
         print(f"Saldo disponível: R${saldo:.2f}")
 
     elif opcao == "2":
-        if saldo != 0 :
+        if saldo != 0:
             saque = float(input(f"Informe o valor do saque: \n"))
-            if saque>saldo:
-                print(f"Saldo insuficiente \n Saldo disponível: R${saldo:.2f}")
+            if saque > saldo:
+                print(f"Saldo insuficiente \nSaldo disponível: R${saldo:.2f}")
             elif numero_saques >= LIMITE_SAQUES:
                 print(f"Limite de saques diários atingido.")
-            elif saque>limite:
-                print(f"""Operação não realizada...
+            elif saque > limite:
+                print(
+                    f"""
+Saque não permitido.
+
 Seu limite para saque é de R${limite:.2f} 
-Saldo disponível: R${saldo:.2f}""")                
-            else: 
+
+Saldo disponível: R${saldo:.2f}
+"""
+                )
+            else:
                 saldo -= saque
-                extrato += f"\nSaque: R${saque:.2f}"
-                numero_saques+=1            
+                extrato += f"\n- Saque: R${saque:.2f}"
+                numero_saques += 1
+                print("\nSaque realizado com sucesso!\n")
                 print(f"Saldo disponível: R${saldo:.2f}")
-        else: 
-            print(f"""Saldo insuficiente
-Saldo disponível: R${saldo:.2f}""") 
+        else:
+            print(
+                f"""
+Saldo insuficiente
+
+Saldo disponível: R${saldo:.2f}
+"""
+            )
 
     elif opcao == "3":
-        if extrato == '' :
+        if extrato == "":
             print("Não foram realizadas movimentações.")
         else:
-            print(f"""Saldo disponível: R${saldo:.2f}
-                  {extrato}""")        
+            print(
+                f"""{extrato}
+                  
+Saldo disponível: R${saldo:.2f}"""
+            )
 
     elif opcao == "0":
         print("Obrigado por utilizar o Banco DIO! Volte sempre!")
         break
 
     else:
-        print(f"Operação inválida, por favor selecione novamente a operação desejada.")
+        print(
+            f"\nOperação inválida, por favor selecione novamente a operação desejada."
+        )
