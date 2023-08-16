c49c46f960f0e0c3a06607ce510a554fcec91423
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-08-16 10:06:58 -0300

    add: cadastrar_cliente

--- desafio.py
+++ desafio.py
@@ -19,7 +19,7 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 is_logged = False
-clientes = set()
+clientes = {}
 contas = {}
 qtd_conta = 0;
 
@@ -51,13 +51,13 @@ def depositar():
 def sacar(): 
     if saldo > 0 and numero_saques < LIMITE_SAQUES:
         saque = float(input(f"Informe o valor do saque: \n"))
-        efetuar_saque(saque)
+        efetuar_saque(saque=saque)
     elif numero_saques >= LIMITE_SAQUES:
         print(f"Limite de saque diário atingido.")
     else:
         print(f"\nSaldo insuficidente\nSaldo atual: R${saldo}")
 
-def efetuar_saque(saque):
+def efetuar_saque(saque=0):
     global saldo
     global numero_saques
     global limite
@@ -78,17 +78,17 @@ Saldo disponível: R${saldo:.2f}
         gerar_extrato("\n- Saque: ", saque)
         numero_saques += 1
         print(f"\nSaque realizado com sucesso!\nSaldo disponível: R${saldo:.2f}")
-        return saldo
 
 def cadastrar_cliente(nome, data_nascimento, cpf, endereco):
     global clientes
-    cpf = {
+    cliente = {
         "nome": nome,
         "data_nascimento": data_nascimento,
-        # "cpf": cpf,
+        "cpf": cpf,
         "endereco": endereco
     }
-    clientes.add(cpf)
+    clientes[cpf]=cliente
+    print("Cliente cadastrado com sucesso.")
 
 def cadastrar_conta(agencia, numero, cliente):
     global contas
@@ -106,70 +106,71 @@ def listar_contas():
     print(contas)
 
 
-# while True:
-#     opcao = input(menu_inicial)
-#     if opcao == '1': #Logar
-#         if not clientes:
-#             print(f"Não há usuários cadastrados.")
-#         else:
-#             is_logged = True
-#             break
-#     elif opcao == '2': #Cadastrar Usuário
-#         print(f"Cadastro de Usuário\n")
-#         cpf = input("CPF: \n")
-#         if cpf in clientes:
-#             print(f"Usuário já cadastrado.")
-#         else:
-#             nome = input("Nome completo: \n")
-#             data_nascimento = input("Data de nascimento: \n")
-#             print(f"Endereço\n")
-#             logradouro = input("Logradouro: \n")
-#             numero = input("Numero: \n")
-#             bairro = input("Bairro: \n")
-#             cidade = input("Cidade: \n")
-#             estado = input("Estado (sigla)")
-#             endereco = {'logradouro': logradouro, 'numero': numero, 'bairro': bairro, 'cidade':cidade, 'estado' : estado }
-#             cadastrar_cliente(nome, data_nascimento, cpf, endereco)
-#     elif opcao == "0": #Sair
-#         print("Obrigado por utilizar o Banco DIO! Volte sempre!")
-#         break
-#     else:
-#         print(
-#         f"\nOperação inválida, por favor selecione novamente a operação desejada."
-#         )
+def exibir_menu_logado():
+    while True:
+        opcao = input(menu_logado)
+
+        if opcao == "1": #Deposito
+            depositar()
+
+        elif opcao == "2": #Saque
+            sacar()
+
+        elif opcao == "3": #extrato
+            exibir_extrato()
+
+        elif opcao == "4": #Cadastrar conta
+            print(f"Cadastro de conta\n")
+            cpf = input("CPF: \n")
+            if cpf in clientes:
+                agencia = input("Agência: \n")
+                qtd_conta+=1
+                cadastrar_conta(agencia, qtd_conta, cpf)
+                  
+        elif opcao == "0": #Sair
+                print("Usuário deslogado...")
+                break
+
+        else:
+                print(
+                    f"\nOperação inválida, por favor selecione novamente a operação desejada."
+                )
+
+while True:
+    opcao = input(menu_inicial)
+    if opcao == '1': #Logar
+        if not clientes:
+            print(f"Não há usuários cadastrados.")
+        else:
+            is_logged = True
+            exibir_menu_logado()
+    elif opcao == '2': #Cadastrar Usuário
+        print(f"Cadastro de Usuário\n")
+        cpf = input("CPF: \n")
+        if cpf in clientes:
+            print(f"Usuário já cadastrado.")
+        else:
+            nome = input("Nome completo: \n")
+            data_nascimento = input("Data de nascimento: \n")
+            print(f"Endereço\n")
+            logradouro = input("Logradouro: \n")
+            numero = input("Numero: \n")
+            bairro = input("Bairro: \n")
+            cidade = input("Cidade: \n")
+            estado = input("Estado (sigla)")
+            endereco = {'logradouro': logradouro, 'numero': numero, 'bairro': bairro, 'cidade':cidade, 'estado' : estado }
+            cadastrar_cliente(nome, data_nascimento, cpf, endereco)
+    elif opcao == "0": #Sair
+        print("Obrigado por utilizar o Banco DIO! Volte sempre!")
+        break
+    else:
+        print(
+        f"\nOperação inválida, por favor selecione novamente a operação desejada."
+        )
 
 
         
     
 
 
-while True:
-    opcao = input(menu_logado)
 
-    if opcao == "1": #Deposito
-        depositar()
-
-    elif opcao == "2": #Saque
-        sacar()
-
-    elif opcao == "3": #extrato
-        exibir_extrato()
-
-    elif opcao == "4": #Cadastrar conta
-        print(f"Cadastro de conta\n")
-        cpf = input("CPF: \n")
-        if cpf in clientes:
-            agencia = input("Agência: \n")
-            qtd_conta+=1
-            cadastrar_conta(agencia, qtd_conta, cpf)
-            
-        
-
-    elif opcao == "0": #Sair
-            print("Obrigado por utilizar o Banco DIO! Volte sempre!")
-            break
-
-    else:
-            print(
-                f"\nOperação inválida, por favor selecione novamente a operação desejada."
-            )
